ff191a6debe5ddece6c780f5f757d5b771d0c85a
emp7yhead <artyomkropp@gmail.com>
2021-12-17 15:23:08 +0900

    add: -f, --format flag

--- gendiff/engine/prog.py
+++ gendiff/engine/prog.py
@@ -1,11 +1,14 @@
 import argparse
 
 DESCRIPTION = 'Generate diff'
-
+FIRST_FILE_VAR = 'first_file'
+SECOND_FILE_VAR = 'second_file'
 
 def parser():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('First file', metavar='first_file')
-    parser.add_argument('Second file', metavar='second_file')
+    parser.add_argument(FIRST_FILE_VAR)
+    parser.add_argument(SECOND_FILE_VAR)
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    print(args.echo)
+    
