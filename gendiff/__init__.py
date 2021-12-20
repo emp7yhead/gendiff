ede09a3f4a35d2b26b529467306afa9da90f6795
emp7yhead <artyomkropp@gmail.com>
2021-12-20 17:00:37 +0900

    fix: linter error

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,1 +1,9 @@
 from gendiff.program.diff_generator import generate_diff
+
+
+def main():
+    generate_diff()
+
+
+if __name__ == '__main__':
+    main()
--- gendiff/program/diff_generator.py
+++ gendiff/program/diff_generator.py
@@ -10,7 +10,7 @@ def generate_diff(file_path1, file_path2):
     second_file = json.load(open(file_path2))
     keys = first_file.keys() | second_file.keys()
     result = {}
-    
+
     for key in keys:
         if key not in first_file:
             result[IN_SECOND + key] = second_file[key]
@@ -21,7 +21,7 @@ def generate_diff(file_path1, file_path2):
         else:
             result[IN_FIRST + key] = first_file[key]
             result[IN_SECOND + key] = first_file[key]
-    
-    result_sorted = {key: result[key] for key in sorted(result, key=lambda x: x[2])}
-    return json.dumps(result_sorted, indent=2, sort_keys=False).replace('"', '')
 
+    result_sorted = {key: result[key] for key in sorted(result,
+                                                        key=lambda x: x[2])}
+    return json.dumps(result_sorted, indent=2, sort_keys=False).replace('"', '')
