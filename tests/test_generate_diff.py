e09ab7e02bcf4bc051a298d94b03f0153bf743fb
emp7yhead <artyomkropp@gmail.com>
2021-12-21 21:01:40 +0900

    add: test for  .yml or .yaml

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,8 +1,14 @@
 import json
 from gendiff.program.diff_generator import generate_diff
 
-diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+file_path1 = 'tests/fixtures/file1.json'
+file_path2 = 'tests/fixtures/file2.json'
+file_path3 = 'tests/fixtures/file1.yaml'
+file_path4 = 'tests/fixtures/file2.yml'
+diff1 = generate_diff(file_path1, file_path2)
+diff2 = generate_diff(file_path3, file_path4)
 expexted_result = '{\n  - follow: false,\n    host: hexlet.io,\n  - proxy: 123.234.53.22,\n  - timeout: 50,\n  + timeout: 50,\n  + verbose: true\n}'
 
 def test_diff():
-    assert diff == expexted_result
+    assert diff1 == expexted_result
+    assert diff2 == expexted_result
