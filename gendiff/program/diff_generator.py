7f07ed61ef31a0755cb21d47df2e968c5ab900d1
emp7yhead <artyomkropp@gmail.com>
2021-12-29 14:07:19 +0900

    refactor diff_generator

--- gendiff/program/diff_generator.py
+++ gendiff/program/diff_generator.py
@@ -1,38 +1,8 @@
-import json
-import yaml
-
-IN_FIRST = '- '
-IN_SECOND = '+ '
-BOTH = '  '
-
-
-def check_file_extension(file_path):
-    if file_path.endswith('.json'):
-        file = json.load(open(file_path))
-    if file_path.endswith(('.yml', '.yaml')):
-        file = yaml.safe_load(open(file_path))
-    return file
+from gendiff.program.diff_creator import parce_data, create_diff
+from gendiff.program.stylish import build_result
 
 
 def generate_diff(file_path1, file_path2):
-    first_file = check_file_extension(file_path1)
-    second_file = check_file_extension(file_path2)
-    # create set of keys in files
-    keys = first_file.keys() | second_file.keys()
-    result = {}
-
-    for key in keys:
-        if key not in first_file:
-            result[IN_SECOND + key] = second_file[key]
-        elif key not in second_file:
-            result[IN_FIRST + key] = first_file[key]
-        elif first_file[key] == second_file[key]:
-            result[BOTH + key] = first_file[key]
-        else:
-            result[IN_FIRST + key] = first_file[key]
-            result[IN_SECOND + key] = first_file[key]
-
-    # sorting result by first letter in key
-    result_sorted = {key: result[key] for key in sorted(result,
-                                                        key=lambda x: x[2])}
-    return json.dumps(result_sorted, indent=2, sort_keys=False).replace('"', '')
+    data1 = parce_data(file_path1)
+    data2 = parce_data(file_path2)
+    return build_result(create_diff(data1, data2))
