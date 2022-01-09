797e021c6737949d1824f6b0b819faf187113358
emp7yhead <artyomkropp@gmail.com>
2022-01-09 23:36:36 +0900

    Update: change arguments

--- gendiff/engine/prog.py
+++ gendiff/engine/prog.py
@@ -10,13 +10,14 @@ FORMAT_HELP = 'set format of output'
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
     parser.add_argument(FORMAT_FLAG_1,
                         FORMAT_FLAG_2,
                         help=FORMAT_HELP,
                         default=diff_generator.FORMAT_STYLISH)
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
     args = parser.parse_args()
-    print(diff_generator.generate_diff(args.first_file,
-                                       args.second_file,
-                                       args.format))
+    diff = diff_generator.generate_diff(args.first_file,
+                                        args.second_file,
+                                        args.format)
+    print(diff)
