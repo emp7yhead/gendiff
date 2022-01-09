70f7bf08a6a1bf5dda64e64020d80b8b8b11283d
emp7yhead <artyomkropp@gmail.com>
2022-01-10 01:38:07 +0900

    Update: refactor diff_creator

--- gendiff/program/diff_creator.py
+++ gendiff/program/diff_creator.py
@@ -9,33 +9,35 @@ def create_diff(data1, data2):
     keys = data1.keys() | data2.keys()
     result = []
     for key in sorted(keys):
-
-        if key not in data1:
-            collected_data = create_diff_segment(VALUE_ADDED,
-                                                 key,
-                                                 data2[key])
-        elif key not in data2:
-            collected_data = create_diff_segment(VALUE_DELETED,
-                                                 key,
-                                                 data1[key])
-        elif data1[key] == data2[key]:
-            collected_data = create_diff_segment(VALUE_UNCHANGED,
-                                                 key,
-                                                 data1[key])
-        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
-            collected_data = create_diff_segment(VALUE_CHILD,
-                                                 key,
-                                                 value1=None,
-                                                 child=create_diff(data1[key],
-                                                                   data2[key]))
-        else:
-            collected_data = create_diff_segment(VALUE_CHANGED,
-                                                 key,
-                                                 value1=data1[key],
-                                                 value2=data2[key])
-        result.append(collected_data)
-
+        result.append(collect_data(key, data1, data2))
     return result
+
+
+def collect_data(key, data1, data2):
+    if key not in data1:
+        collected_data = create_diff_segment(VALUE_ADDED,
+                                             key,
+                                             data2[key])
+    elif key not in data2:
+        collected_data = create_diff_segment(VALUE_DELETED,
+                                             key,
+                                             data1[key])
+    elif data1[key] == data2[key]:
+        collected_data = create_diff_segment(VALUE_UNCHANGED,
+                                             key,
+                                             data1[key])
+    elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+        collected_data = create_diff_segment(VALUE_CHILD,
+                                             key,
+                                             value1=None,
+                                             child=create_diff(data1[key],
+                                                               data2[key]))
+    else:
+        collected_data = create_diff_segment(VALUE_CHANGED,
+                                             key,
+                                             value1=data1[key],
+                                             value2=data2[key])
+    return collected_data
 
 
 def create_diff_segment(status, key, value1, value2=None, child=None):
