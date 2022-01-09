fb763cc4260193e83a277e1e811b47964096547d
emp7yhead <artyomkropp@gmail.com>
2022-01-09 23:37:23 +0900

    add: json format to diff generator

--- gendiff/program/diff_generator.py
+++ gendiff/program/diff_generator.py
@@ -2,9 +2,11 @@ from gendiff.program.data_parcer import parce_data
 from gendiff.program.diff_creator import create_diff
 from gendiff.formatter.stylish import build_stylish
 from gendiff.formatter.plain import build_plain
+from gendiff.formatter.json import build_json
 
 FORMAT_STYLISH = 'stylish'
 FORMAT_PLAIN = 'plain'
+FORMAT_JSON = 'json'
 
 ERROR = 'Please choose valid format.'
 
@@ -16,5 +18,7 @@ def generate_diff(file_path1, file_path2, style=FORMAT_STYLISH):
         return build_stylish(create_diff(data1, data2))
     elif style == FORMAT_PLAIN:
         return build_plain(create_diff(data1, data2))
+    elif style == FORMAT_JSON:
+        return build_json(create_diff(data1, data2))
     else:
         raise NameError(ERROR)
