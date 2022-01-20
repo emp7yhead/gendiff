b84e7d681e01cf01cb2aaabfe09a3bde2b093725
emp7yhead <artyomkropp@gmail.com>
2022-01-20 20:32:55 +0900

    Update: tests for data_loader

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,22 +1,16 @@
 import pytest
 from gendiff.diff_builder import data_loader
 
-
-def test_get_file_data_json():
-    test_case = 'tests/fixtures/nested_1.json'
-    data, extension = data_loader.get_file_data_and_extension(test_case)
-    expected_data = open(test_case, 'r')
-    assert data, extension == (expected_data, '.json')
-
-
-def test_get_file_data_yaml():
-    test_case = 'tests/fixtures/nested_1.yml'
-    data, extension = data_loader.get_file_data_and_extension(test_case)
-    expected_data = open(test_case, 'r')
-    assert data, extension == (expected_data, ('.yml', '.yaml'))
+TEST_TEMPLATE = [
+    ('tests/fixtures/nested_1.json', '.json'),
+    ('tests/fixtures/nested_1.yml', ('.yml', '.yaml')),
+    pytest.param('tests/fixtures/nested_1.psd',
+                 '.psd',
+                 marks=pytest.mark.xfail)
+    ]
 
 
-@pytest.mark.xfail
-def test_get_file_data_fail():
-    test_case = 'tests/fixtures/nested_1.psd'
-    assert test_case
+@pytest.mark.parametrize('test_case, expected_result', TEST_TEMPLATE)
+def test_get_file_extension(test_case, expected_result):
+    extension = data_loader.get_file_extension(test_case)
+    assert extension == expected_result
