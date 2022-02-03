bb8f2d5ffab8e0bf4d5693128cd027530369db63
emp7yhead <artyomkropp@gmail.com>
2022-02-03 18:13:15 +0900

    Update: naming and docstrings

--- gendiff/cli/cli.py
+++ gendiff/cli/cli.py
@@ -1,19 +1,30 @@
+"""Function to parse arguments in CLI."""
 import argparse
 
 from gendiff.diff_builder import diff_generator
 
 DESCRIPTION = 'Generate diff'
-FORMAT_FLAG_1 = '-f'
-FORMAT_FLAG_2 = '--format'
+FORMAT_FLAG1 = '-f'
+FORMAT_FLAG2 = '--format'
 FORMAT_HELP = 'set format of output'
 
 
 def parse_arguments():
+    """
+    Parse arguments for gendiff.
+
+    Returns:
+        args.first_file: str
+        args.second_file: str
+        args.format: str
+    """
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument(FORMAT_FLAG_1,
-                        FORMAT_FLAG_2,
-                        help=FORMAT_HELP,
-                        default=diff_generator.FORMAT_STYLISH)
+    parser.add_argument(
+        FORMAT_FLAG1,
+        FORMAT_FLAG2,
+        help=FORMAT_HELP,
+        default=diff_generator.FORMAT_STYLISH,
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
