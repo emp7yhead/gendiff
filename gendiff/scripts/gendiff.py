16afb606b22223db299d2bd9e2dfdd16a8035bf9
emp7yhead <artyomkropp@gmail.com>
2022-02-04 19:02:53 +0900

    Update: docstrings

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,17 @@
+"""Main script of gendiff."""
 from gendiff.cli import cli
 from gendiff.diff_builder import diff_generator
 
 
-def main():
-    first_file, second_file, format = cli.parse_arguments()
-    rendered_diff = diff_generator.generate_diff(first_file,
-                                                 second_file,
-                                                 format)
-    print(rendered_diff)
+def main() -> None:
+    """Print diff between two files."""
+    first_file, second_file, style_format = cli.parse_arguments()
+    rendered_diff = diff_generator.generate_diff(
+        first_file,
+        second_file,
+        style_format,
+    )
+    print(rendered_diff)  # noqa: WPS421
 
 
 if __name__ == '__main__':
