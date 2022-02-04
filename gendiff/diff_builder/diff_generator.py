efdbac913ae4220878942efa394d4a3c3e6da432
emp7yhead <artyomkropp@gmail.com>
2022-02-04 19:01:59 +0900

    Update: docstrings in diff_generator.py

--- gendiff/diff_builder/diff_generator.py
+++ gendiff/diff_builder/diff_generator.py
@@ -1,10 +1,26 @@
+"""Function to generate diff between two files."""
+
 from gendiff.diff_builder.data_loader import get_file_data_and_extension
 from gendiff.diff_builder.data_parser import parse_data
 from gendiff.diff_builder.diff_creator import create_diff
-from gendiff.formatter.renderer import render_diff, FORMAT_STYLISH
+from gendiff.formatter.renderer import FORMAT_STYLISH, render_diff
 
 
-def generate_diff(file_path1, file_path2, style=FORMAT_STYLISH):
+def generate_diff(  # noqa: WPS210
+    file_path1: str,
+    file_path2: str,
+    style: str = FORMAT_STYLISH,
+) -> str:  # noqa: WPS210
+    """Generate diff between two files using chosen style.
+
+    Args:
+        file_path1: absolute or relative path to first file.
+        file_path2: absolute or relative path to second file.
+        style: format of diff(stylish, plain or json, default: stylish)
+
+    Returns:
+        str
+    """
     data1, extension1 = get_file_data_and_extension(file_path1)
     data2, extension2 = get_file_data_and_extension(file_path2)
 
