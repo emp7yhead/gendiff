5c903dca4156fc3a645876080bb92f60edef7a71
emp7yhead <artyomkropp@gmail.com>
2022-02-04 19:04:49 +0900

    Update: docstring

--- gendiff/formatter/renderer.py
+++ gendiff/formatter/renderer.py
@@ -1,6 +1,9 @@
-from gendiff.formatter.stylish import build_stylish
-from gendiff.formatter.plain import build_plain
+"""Function to render diff in chosen format."""
+from typing import Dict, List
+
 from gendiff.formatter.json import build_json
+from gendiff.formatter.plain import build_plain
+from gendiff.formatter.stylish import build_stylish
 
 FORMAT_STYLISH = 'stylish'
 FORMAT_PLAIN = 'plain'
@@ -9,12 +12,23 @@ FORMAT_JSON = 'json'
 FORMAT_ERROR = 'Please choose valid format.'
 
 
-def render_diff(diff, style):
+def render_diff(diff: List[Dict], style: str) -> str:
+    """Render diff in chosen format.
+
+    Args:
+        diff: diff between two files.
+        style: format of diff(stylish, plain or json)
+
+    Returns:
+        str.
+
+    Raises:
+        NameError: if style is not supported.
+    """
     if style == FORMAT_STYLISH:
         return build_stylish(diff)
     elif style == FORMAT_PLAIN:
         return build_plain(diff)
     elif style == FORMAT_JSON:
         return build_json(diff)
-    else:
-        raise NameError(FORMAT_ERROR)
+    raise NameError(FORMAT_ERROR)
